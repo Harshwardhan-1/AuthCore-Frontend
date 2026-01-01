4d1ec6437699756dfe33e829214075db4ed5b0cf
harsh <harshwardhany87@gmail.com>
2026-01-01 13:58:14 +0530

    add SignInPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import {Routes,Route} from 'react-router-dom';
 import SignUpPage from "./components/SignUpPage"
+import SignInPage from './components/SignInPage';
 function App() {
-
   return (
     <>
     <Routes>
       <Route path='/' element={<SignUpPage />}></Route>
+      <Route path='/signIn' element={<SignInPage />}></Route>
     </Routes>
     </>
   )
---
+++ frontend/src/components/SignInPage.jsx
@@ -0,0 +1,5 @@
+export default function SignInPage(){
+    return(
+        <h1>Hii welcome to sign in page</h1>
+    );
+}
