b114dde8102c7ff45cc36d5094ef5f118d817547
harsh <harshwardhany87@gmail.com>
2026-01-01 12:39:48 +0530

    AuthCore-Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {Routes,Route} from 'react-router-dom';
+import SignUpPage from "./components/SignUpPage"
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<SignUpPage />}></Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
