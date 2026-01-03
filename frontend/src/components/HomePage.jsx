3bc39063eb2e850ef4753f99cc5edaf8a26a8260
harsh <harshwardhany87@gmail.com>
2026-01-03 10:17:19 +0530

    add UserData

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,4 +1,7 @@
 export default function HomePage({userData}){
+     if (!userData || !userData.name) {
+    return <h1>Loading user data... please login first</h1>;
+  }
     return(
         <>
         <h1>Welcome to HomePage</h1>
