cef026a8f1c9a692ad67998528b5014ebcf22966
harsh <harshwardhany87@gmail.com>
2026-01-03 20:42:04 +0530

    change password

--- frontend/src/components/NewPassword.jsx
+++ frontend/src/components/NewPassword.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 export default function NewPassword(){
+    const navigate=useNavigate();
+    const [password,setPassword]=useState("");
+    const [confirmPassword,setConfirmPassword]=useState("");
+    const handle=async(e)=>{
+        e.preventDefault();
+        const send={password,confirmPassword};
+        try{
+        const response=await axios.post("https://authcore-backend-3.onrender.com/api/all/changePassword",send,{withCredentials:true})
+        if(response.data.message==="change successfull"){
+            navigate('/signIn');
+        }
+        }catch(err){
+            if(err.response?.data?.message==='enter detail properly'){
+                alert('fill detail properly');
+            }else if(err.response?.data?.message==='fill your detail properly'){
+                alert('password and incorrect password does not match');
+            }else if(err.response?.data?.message==='user not found'){
+                alert('do a sign up first');
+                navigate('/');
+            }
+        }
+    }
     return(
         <>
         <h1>Enter your new Password here</h1>
+        <form onSubmit={handle}>
+ <input type="password" placeholder="Enter your newPassword here" onChange={(e)=>setPassword(e.target.value)}/>
+ <input type="password" placeholder="Enter your confirmPassword here" onChange={(e)=>setConfirmPassword(e.target.value)}/>
+ <button type="submit">click here</button>
+        </form>
         </>
     );
 }
