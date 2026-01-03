36b45a2aed5fd3cd06dd092658c9b118f1087c48
harsh <harshwardhany87@gmail.com>
2026-01-03 23:07:15 +0530

    minor fix

--- frontend/src/components/SignInPage.jsx
+++ frontend/src/components/SignInPage.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './SignInPage.css'
 export default function SignInPage({setUserData}){
     const navigate=useNavigate();
