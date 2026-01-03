c3f54919e9640f9e1f9227d328f6215cbef0849b
harsh <harshwardhany87@gmail.com>
2026-01-03 22:26:17 +0530

    make small changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route, UNSAFE_useFogOFWarDiscovery} from 'react-router-dom';
 import SignUpPage from "./components/SignUpPage"
 import SignInPage from './components/SignInPage';
 import HomePage from './components/HomePage';
@@ -6,7 +6,25 @@ import ForgotPassword from './components/ForgotPassword';
 import OtpVerify from './components/OtpVerify';
 import NewPassword from './components/NewPassword';
 import {useState} from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 function App() {
+  const navigate=useNavigate();
+  useEffect(()=>{
+    const fetch=async()=>{
+      try{
+  const response=await axios.get('https://authcore-backend-3.onrender.com/api/all/authCheck',{withCredentials:true});
+  if(response.data.message=== 'Authorized'){
+    navigate('/HomePage');
+  }
+    }catch(err){
+      navigate('/signIn');
+      console.log(err);
+    }
+    };
+    fetch();
+  },[navigate]);
 
   const [userData,setUserData]=useState(null);
   const [passwordData,setPasswordData]=useState(null);
