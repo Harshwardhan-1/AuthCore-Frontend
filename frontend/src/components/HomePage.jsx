dd28b31a749462787727093544254584726ff125
harsh <harshwardhany87@gmail.com>
2026-01-03 09:57:43 +0530

    add vercel.json

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,7 +2,6 @@ export default function HomePage({userData}){
     return(
         <>
         <h1>Welcome to HomePage</h1>
-        <p>{userData.name}</p>
         <p>{userData.gmail}</p>
         </>
     );
