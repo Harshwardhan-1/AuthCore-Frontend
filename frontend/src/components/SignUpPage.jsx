6da57f38d49c62684deebbf07bedf89466cb758c
harsh <harshwardhany87@gmail.com>
2026-01-03 11:28:44 +0530

    change style

--- frontend/src/components/SignUpPage.jsx
+++ frontend/src/components/SignUpPage.jsx
@@ -28,7 +28,7 @@ export default function SignUpPage(){
         <>
             <div className="signup-page-container">
             <div className="signup-form-wrapper">
-        <form onSubmit={handle}>
+        <form onSubmit={handle} className="signup-form">
             <input type="text" placeholder="Enter your name here"  onChange={(e)=>setName(e.target.value)}/>
             <input type="email" placeholder="Enter your email here" onChange={(e)=>setGmail(e.target.value)} />
             <input type="password" placeholder="Enter your password here"  onChange={(e)=>setPassword(e.target.value)} />
